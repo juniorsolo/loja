e8ed6f8e943024307bc400251fe8b10a43162338
juniorsolo <junior.solo@hotmail.com>
2018-04-24 21:05:06 -0300

    Mudando chamada do serviço para path paramter.

--- src/test/java/br/com/alura/loja/ClienteTest.java
+++ src/test/java/br/com/alura/loja/ClienteTest.java
@@ -35,7 +35,7 @@ public class ClienteTest {
 		//WebTarget target =  cliente.target("http://www.mocky.io/");
 		//String conteudo = target.path("v2/5ade8c292f0000650056de47").request().get(String.class);
 		WebTarget target =  cliente.target("http://localhost:8080");
-		String conteudo = target.path("/carrinhos").request().get(String.class);
+		String conteudo = target.path("/carrinhos/1").request().get(String.class);
 		Carrinho c = (Carrinho) new XStream().fromXML(conteudo);
 		Assert.assertEquals("Rua Vergueiro 3185, 8 andar", c.getRua());
 		
